9eac3409459252e2e61df2c37100bea002445f6e
asifvora <asifvora0@gmail.com>
2023-03-21 11:36:34 +0530

    New version release

--- packages/current-page-fallback/src/hooks/usePageRoute.ts
+++ packages/current-page-fallback/src/hooks/usePageRoute.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { FallbackContext, FallbackType } from '../providers/FallbackProvider';
 
 export const usePageRoute = (): any => {
--- packages/current-page-fallback/src/providers/FallbackProvider.tsx
+++ packages/current-page-fallback/src/providers/FallbackProvider.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import ProgressBar from 'react-topbar-progress-indicator';
 
 // Progress Bar -> Configuration
